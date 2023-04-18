e8b9529ebbc0c65a0be82c1feef87d0405907f8e
jolbol1 <github@jamesshopland.com>
2023-04-18 12:59:50 +0100

    🐛 fix ignored case

---
+++ components/tags.tsx
@@ -0,0 +1,29 @@
+import Link from '@/components/link'
+import Tag from '@/components/tag'
+import { kebabCase } from '../utils/kebabCase'
+
+export default function Tags({ tags }) {
+  const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
+  return (
+    <div className="flex  flex-wrap gap-2">
+      {Object.keys(tags).length === 0 && 'No tags found.'}
+      {sortedTags.map((t) => {
+        return (
+          <div
+            key={t}
+            className="2xl rounded-lg bg-blue-600  px-2 py-1 text-sm text-white hover:scale-110 hover:bg-blue-600 dark:bg-blue-950 "
+          >
+            <Tag text={t} className="mr-3 text-sm font-medium lowercase  " />
+            <Link
+              href={`/tags/${kebabCase(t)}`}
+              className="-ml-2 text-sm font-semibold uppercase text-white"
+              aria-label={`View posts tagged ${t}`}
+            >
+              {` (${tags[t]})`}
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
