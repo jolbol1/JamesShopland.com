e28178cfc51c73f553e154d9f6d930ea8d697458
jolbol1 <github@jamesshopland.com>
2023-04-20 21:06:38 +0100

    :sparkles: custom page not found

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -44,6 +44,7 @@ export default function About() {
               width={192}
               height={192}
               className="h-48 w-48 rounded-full"
+              priority={true}
             />
           )}
           <h2 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h2>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,27 @@
 import Link from '@/components/link'
+import FourZeroFour from '@/public/images/general/404.png'
+import Image from 'next/image'
 
-export default function FourZeroFour() {
+export default function NotFound() {
   return (
     <>
-      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-            404
-          </h1>
-        </div>
-        <div className="max-w-md">
+      <div className="flex flex-col items-center">
+        <Image
+          priority={true}
+          src={FourZeroFour}
+          height={320}
+          alt={'cartoon james shopland looking confused'}
+        />
+        <h1 className="mt-6 w-full text-center text-6xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100  md:text-8xl">
+          404
+        </h1>
+        <div className="flex max-w-md flex-col">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
-          </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
+            Sorry this page could not be found
           </p>
           <Link
             href="/"
-            className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+            className="focus:shadow-outline-blue mt-6 inline w-full rounded-lg border border-transparent bg-blue-600 px-4 py-4 text-center text-lg font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
           >
             Back to homepage
           </Link>
