ee4939c704aea9680b744e7d7111f8bd18a8c650
jolbol1 <github@jamesshopland.com>
2023-04-21 19:36:04 +0100

    :bug: fix tag routing error

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { slug } from 'github-slugger'
 
 export interface TagProps {
   text: string
@@ -8,7 +9,7 @@ export interface TagProps {
 const Tag = ({ text, className }: TagProps) => {
   return (
     <Link
-      href={`/tags/${text}`}
+      href={`/tags/${slug(text)}`}
       className={
         className ??
         'mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
