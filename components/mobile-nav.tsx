fc2538e97bf69ece87ea26aadcf456421c9c8ef4
jolbol1 <github@jamesshopland.com>
2023-07-03 21:46:32 +0100

    ✨ add navbar mobile

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -10,94 +10,76 @@ import headerNavLinks from '@/config/nav-links'
 import { GlowDiv } from './glow-div'
 import Image from 'next/image'
 import Logo from '@/public/images/general/logo.webp'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from './ui/navigation-menu'
+import * as NavigationMenuRadix from '@radix-ui/react-navigation-menu'
+
+import React from 'react'
+import { Menu, X } from 'lucide-react'
 
 const MobileNav = () => {
   const [navShow, setNavShow] = useState(false)
 
   return (
     <div className="[@media(min-width:700px)]:hidden">
-      <Sheet open={navShow} onOpenChange={setNavShow}>
-        <SheetTrigger asChild>
-          <button
-            className=" h-[34px] w-[34px] rounded border border-gray-800 bg-gray-200 p-1.5 dark:border-gray-200 dark:bg-gray-900"
-            aria-label="Toggle Menu"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="text-gray-900 dark:text-gray-100"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </SheetTrigger>
-
-        <SheetContent>
-          <MobileLink href="/" className="flex items-center" onOpenChange={setNavShow}>
-            <div className="flex items-center justify-between align-middle">
-              <div className="mr-3">
-                <GlowDiv>
-                  <Image src={Logo} height={48} width={48} alt="James Shopland Logo" />
-                </GlowDiv>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuRadix.Trigger className="group">
+              <div
+                className="flex h-[34px] w-[34px] items-center justify-center rounded border  border-gray-800 bg-gray-200 p-1.5 dark:border-gray-200 dark:bg-gray-900"
+                aria-label="Toggle Menu"
+              >
+                <Menu className="group-data-[state='open']:hidden" />
+                <X className="hidden group-data-[state='open']:block" />
               </div>
-              {typeof siteMetadata.headerTitle === 'string' ? (
-                <div className="underlined after:bottom[0px]  text-xl font-semibold   [@media(min-width:440px)]:text-2xl">
-                  {siteMetadata.headerTitle}
-                </div>
-              ) : (
-                siteMetadata.headerTitle
-              )}
-            </div>
-          </MobileLink>
-          <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
-            <div className="mt-3 flex flex-col space-y-6">
-              {headerNavLinks?.map(
-                (item) =>
-                  item.href && (
-                    <MobileLink
-                      key={item.href}
-                      href={item.href}
-                      onOpenChange={setNavShow}
-                      className="mt-3 text-2xl font-semibold"
-                    >
-                      {item.title}
-                    </MobileLink>
-                  )
-              )}
-            </div>
-          </ScrollArea>
-        </SheetContent>
-      </Sheet>
+            </NavigationMenuRadix.Trigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px] ">
+                {headerNavLinks.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  ></ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   )
 }
 
-interface MobileLinkProps extends LinkProps {
-  onOpenChange?: (open: boolean) => void
-  children: React.ReactNode
-  className?: string
-}
-
-function MobileLink({ href, onOpenChange, className, children, ...props }: MobileLinkProps) {
-  const router = useRouter()
-  return (
-    <Link
-      href={href}
-      onClick={() => {
-        router.push(href.toString())
-        onOpenChange?.(false)
-      }}
-      className={cn(className)}
-      {...props}
-    >
-      {children}
-    </Link>
-  )
-}
+const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+              className
+            )}
+            {...props}
+          >
+            <div className="text-2xl font-semibold leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    )
+  }
+)
+ListItem.displayName = 'ListItem'
 
 export default MobileNav
