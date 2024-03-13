4d81450d6585d03a215145a956a1cdc78190d2f7
jolbol1 <github@jamesshopland.com>
2024-03-13 16:32:18 +0000

    fix: layout changes

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ import siteMetadata from "@/config/site-metadata"
 export function Header() {
   return (
     <section className="relative ">
-      <div className="mx-auto mb-24 grid h-auto max-w-7xl grid-cols-4 gap-x-4 pt-0 md:grid-cols-8 lg:relative lg:mb-64 lg:min-h-[40rem] lg:grid-cols-12 lg:gap-x-6 lg:pb-12 lg:pt-24">
+      <div className="mx-auto mb-24 grid h-auto max-w-7xl auto-rows-min grid-cols-4 gap-x-4 pt-0 md:grid-cols-8 lg:relative lg:mb-64 lg:min-h-[40rem] lg:grid-cols-12 lg:gap-x-6 lg:pb-12 lg:pt-24">
         <div className="col-span-full pt-6 lg:col-span-5 lg:col-start-1 lg:row-start-1 lg:flex lg:h-full lg:flex-col">
           <h1 className="typing-demo mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
             Hello, I'm James
@@ -35,7 +35,7 @@ export function Header() {
           </div>
         </div>
         <div className="col-span-full mb-12 flex items-center justify-center lg:col-span-7 lg:col-start-6 lg:-mr-[5vw] lg:-mt-24 lg:mb-0 lg:px-0">
-          <div className="relative">
+          <div className="relative w-full">
             <Image
               src={hero}
               quality={50}
@@ -43,7 +43,7 @@ export function Header() {
                 "cartoon image of james shopland with icons of programming languages surrounding him"
               }
               priority
-              className="h-auto max-h-[50vh] w-full object-contain lg:max-h-[75vh]"
+              className="w-full max-h-[50vh] object-contain lg:max-h-[75vh]"
               sizes="(max-width: 1024px) 60vw,
               100vw"
             />
--- components/search/search-bar-button.tsx
+++ components/search/search-bar-button.tsx
@@ -13,9 +13,9 @@ export const SearchBarButton = ({
     <button
       onClick={onClick}
       title="Search"
-      className="mx-4 flex h-[34px] w-[34px] cursor-text items-center justify-center rounded-md border border-gray-800 bg-gray-50 px-2 text-sm hover:border-blue-600 hover:bg-gray-100 dark:border-gray-200 dark:bg-gray-900 dark:hover:border-blue-600 dark:hover:bg-gray-800 [@media(min-width:810px)]:w-[unset]"
+      className="mx-4 flex h-[34px] w-[34px] cursor-text items-center justify-center rounded-md border border-gray-800 bg-gray-50 px-2 text-sm hover:border-blue-600 hover:bg-gray-100 dark:border-gray-200 dark:bg-gray-900 dark:hover:border-blue-600 dark:hover:bg-gray-800 [@media(min-width:900px)]:w-[unset]"
     >
-      <span className="my-2 block  w-4 [@media(min-width:810px)]:mr-2">
+      <span className="my-2 block  w-4 [@media(min-width:900px)]:mr-2">
         <svg
           className="text-gray-800 dark:text-gray-300"
           xmlns="http://www.w3.org/2000/svg"
@@ -31,16 +31,16 @@ export const SearchBarButton = ({
           />
         </svg>
       </span>
-      <span className="mr-8 hidden text-slate-600 dark:text-slate-300 min-[900px]:block">
+      <span className="mr-8 hidden text-slate-600 dark:text-slate-300 [@media(min-width:980px)]:block">
         Search...
       </span>
       <kbd
-        className={`hidden  whitespace-nowrap rounded px-1 align-middle font-medium leading-4 tracking-wide [font-size:10px] [@media(min-width:810px)]:inline-block ${"border border-slate-400/70 text-slate-500 dark:border-slate-600 dark:text-slate-400"}`}
+        className={`hidden  whitespace-nowrap rounded px-1 align-middle font-medium leading-4 tracking-wide [font-size:10px] [@media(min-width:900px)]:inline-block ${"border border-slate-400/70 text-slate-500 dark:border-slate-600 dark:text-slate-400"}`}
       >
         {kbd}
       </kbd>
       <kbd
-        className={`ml-1 hidden whitespace-nowrap rounded px-1 pl-1 align-middle font-medium leading-4 tracking-wide [font-size:10px] [@media(min-width:810px)]:inline-block ${"border border-slate-400/70 text-slate-500 dark:border-slate-600 dark:text-slate-400"}`}
+        className={`ml-1 hidden whitespace-nowrap rounded px-1 pl-1 align-middle font-medium leading-4 tracking-wide [font-size:10px] [@media(min-width:900px)]:inline-block ${"border border-slate-400/70 text-slate-500 dark:border-slate-600 dark:text-slate-400"}`}
       >
         K
       </kbd>
