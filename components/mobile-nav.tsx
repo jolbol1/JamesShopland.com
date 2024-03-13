f3d154968334ee311ace35fa666d665453322c00
jolbol1 <github@jamesshopland.com>
2024-03-13 17:26:59 +0000

    fix: mobile nav display

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -43,7 +43,7 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed left-0 top-0 z-10 size-full bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-black${
+        className={`fixed left-0 top-0 z-10 size-full bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-black ${
           navShow ? "translate-x-0" : "translate-x-full"
         }`}
       >
